156dadc5891efeef88abdff88d28d38860e4b766
Mariana Junita <ymawuntu14@gmail.com>
2025-03-04 14:55:06 +0700

    Update api service

--- api/service.php
+++ api/service.php
@@ -1,13 +1,8 @@
 <?php
 require("autoload.php");
 
-$stmt = "SELECT s.`nomor_rangka`, f.`tanggal_beli_motor` FROM `service` AS s LEFT JOIN `faktur` AS f ON s.`nomor_rangka` = f.`nomor_rangka`";
+$stmt = "SELECT s.`nomor_rangka`, f.`tanggal_beli_motor` FROM `service` AS s INNER JOIN `faktur` AS f ON s.`nomor_rangka` = f.`nomor_rangka`";
 $result = $conn->query($stmt);
-
-if (!$result) {
-    die("Query failed: " . $conn->error);
-}
-
 $row = $result->fetch_all(MYSQLI_ASSOC);
 
 echo json_encode($row);
